f75195c8a2b6929d359f8e6795f972fb544093b6
jackylovechina <jackylovechina@gmail.com>
2016-10-19 09:20:56 +0800

    工时管理软件git项目，完成登录界面的布局UI设计,以及自动登录和记住密码相关UI，完成登录界面转换

--- app/src/main/java/com/shg/manhourapp/LoginActivity.java
+++ app/src/main/java/com/shg/manhourapp/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.shg.manhourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView Login_TV;
+    private EditText UserName_ET;
+    private EditText PassWord_ET;
+    private CheckBox Auto_CB;
+    private CheckBox RemB_CB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Login_TV = (TextView) findViewById(R.id.tv_login_in);
+
+        Login_TV.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.tv_login_in:
+                Intent intent = new Intent();
+                intent.setClass(this, DispatchListActivity.class);
+                startActivity(intent);
+                this.finish();
+
+                break;
+        }
+
     }
 }
