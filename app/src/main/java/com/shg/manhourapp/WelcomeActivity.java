8aa62a3ec9c18e814afa709ec23b9b0f3402b1ab
jackylovechina <jackylovechina@gmail.com>
2016-10-21 13:07:44 +0800

    工时管理软件git项目，完成工时管理软件手机端APP欢迎界面倒计时背景和登陆界面登陆按键背景的优化

--- app/src/main/java/com/shg/manhourapp/WelcomeActivity.java
+++ app/src/main/java/com/shg/manhourapp/WelcomeActivity.java
@@ -1,18 +1,58 @@
 package com.shg.manhourapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static android.R.attr.handle;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView WelcomeNum_TV;
+
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            WelcomeNum_TV.setText(msg.what + "s");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        WelcomeNum_TV = (TextView) findViewById(R.id.tv_welcome_num);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 2; i >= 0; i--) {
+
+                    try {
+                        Thread.sleep(1000);
+                        Message msg = new Message();
+                        msg.what = i;
+
+                        handler.sendEmptyMessage(msg.what);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
 
         Timer timer = new Timer();
         TimerTask timerTask = new TimerTask() {
@@ -27,6 +67,6 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         };
 
-        timer.schedule(timerTask, 2000);
+        timer.schedule(timerTask, 3000);
     }
 }
